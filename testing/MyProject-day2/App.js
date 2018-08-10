09250377267d4800733cc06903013df2c12c18cb
Bob Brown <robertbernardbrown@gmail.com>
2018-08-10 13:56:14 -0400

    practices animations

--- testing/MyProject-day2/App.js
+++ testing/MyProject-day2/App.js
@@ -2,34 +2,49 @@ import React, {Component} from 'react';
 import {Animated, CameraRoll, Platform, FlatList, StyleSheet, Text, View, Button, TouchableHighlight, TextInput} from 'react-native';
 
 export default class App extends Component {
-  animatedValue = new Animated.Value(0)
+  animatedValue1 = new Animated.Value(0)
+  animatedValue2 = new Animated.Value(0)
+  animatedValue3 = new Animated.Value(0)
 
   animate = () => {
-    Animated.timing(
-      this.animatedValue, 
-      {toValue:1, duration:1500}
-    ).start()
+    this.animatedValue1.setValue(0)
+    this.animatedValue2.setValue(0)
+    this.animatedValue3.setValue(0)
+    const createAnimation = (value, toValue, duration) => Animated.timing(
+      value, 
+      {
+        toValue, 
+        duration
+      }
+    )
+    Animated.parallel([
+      createAnimation(this.animatedValue1, 100, 500),
+      createAnimation(this.animatedValue2, 700, 1000),
+      createAnimation(this.animatedValue3, 350, 1500)
+    ]).start((()=> {
+      Animated.parallel([
+        createAnimation(this.animatedValue1, 0, 500),
+        createAnimation(this.animatedValue2, 0, 1000),
+        createAnimation(this.animatedValue3, 0, 1500)
+      ]).start(this.animate)
+    }))
   }
 
   render() {
-    const margin = this.animatedValue.interpolate({
-      inputRange: [0, 1],
-      outputRange: [0, 200]
-    })
-    const backgroundColor = this.animatedValue.interpolate({
-      inputRange: [0, 1],
-      outputRange: ["red", "orange"]
-    })
-    const rotation = this.animatedValue.interpolate({
-      inputRange: [0, 1],
-      outputRange: ["0deg", "360deg"]
-    })
+    
     return (
       <View style={styles.container}>
         <Text>Animations:</Text>
         <Button onPress={this.animate} title="Animate"/>
-        <Animated.View style={{ transform:[{rotate:rotation}], height:50, width:50, backgroundColor, marginTop:margin, marginLeft:margin}}/>
-        <Animated.Text style={{fontSize: this.animatedValue}}>FONT</Animated.Text>
+        <Animated.View style={{ 
+          height:50, width:50, backgroundColor: "red", marginTop:this.animatedValue1
+          }}/>
+        <Animated.View style={{ 
+          height:50, width:50, backgroundColor: "orange", marginLeft:this.animatedValue2
+          }}/>
+        <Animated.View style={{ 
+          height:50, width:50, backgroundColor: "blue", marginTop:this.animatedValue3
+          }}/>
       </View>
     );
   }
