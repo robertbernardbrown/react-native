f8a4dd6531b16527ff7daf897cb02db29332c4b0
Bob Brown <robertbernardbrown@gmail.com>
2018-08-10 17:06:56 -0400

    adds navigation and authorization to demo

--- testing/MyProject-day2/App.js
+++ testing/MyProject-day2/App.js
@@ -1,54 +1,51 @@
 import React, {Component} from 'react';
 import {Animated, CameraRoll, Platform, FlatList, StyleSheet, Text, View, Button, TouchableHighlight, TextInput} from 'react-native';
+import {createStackNavigator, createSwitchNavigator, createDrawerNavigator, createBottomTabNavigator} from "react-navigation";
 
-export default class App extends Component {
-  animatedValue1 = new Animated.Value(0)
-  animatedValue2 = new Animated.Value(0)
-  animatedValue3 = new Animated.Value(0)
+import {withAuthenticator} from "aws-amplify-react-native";
 
-  animate = () => {
-    this.animatedValue1.setValue(0)
-    this.animatedValue2.setValue(0)
-    this.animatedValue3.setValue(0)
-    const createAnimation = (value, toValue, duration) => Animated.timing(
-      value, 
-      {
-        toValue, 
-        duration
-      }
-    )
-    Animated.parallel([
-      createAnimation(this.animatedValue1, 100, 500),
-      createAnimation(this.animatedValue2, 700, 1000),
-      createAnimation(this.animatedValue3, 350, 1500)
-    ]).start((()=> {
-      Animated.parallel([
-        createAnimation(this.animatedValue1, 0, 500),
-        createAnimation(this.animatedValue2, 0, 1000),
-        createAnimation(this.animatedValue3, 0, 1500)
-      ]).start(this.animate)
-    }))
-  }
+class App extends Component {
 
   render() {
     
     return (
       <View style={styles.container}>
-        <Text>Animations:</Text>
-        <Button onPress={this.animate} title="Animate"/>
-        <Animated.View style={{ 
-          height:50, width:50, backgroundColor: "red", marginTop:this.animatedValue1
-          }}/>
-        <Animated.View style={{ 
-          height:50, width:50, backgroundColor: "orange", marginLeft:this.animatedValue2
-          }}/>
-        <Animated.View style={{ 
-          height:50, width:50, backgroundColor: "blue", marginTop:this.animatedValue3
-          }}/>
+        <Text>Navigation:</Text>
+        <Text>You're logged in</Text>
       </View>
     );
   }
 }
+
+// const Unauthorized = props => (
+//   <View>
+//     <Text>Hello from you're unauthorized</Text>
+//   </View>
+// )
+
+// class Initializing extends Component {
+//   componentDidMount() {
+//     setTimeout(()=> {
+//       this.props.navigation.navigate("Authorized")
+//     }, 3000)
+//   }
+//   render() {
+    
+//     return (
+//       <View style={styles.container}>
+//         <Text>Checking status</Text>
+//       </View>
+//     );
+//   }
+// }
+
+// const Authorized = createStackNavigator({App:{screen:App}})
+
+// const MainNav = createSwitchNavigator({
+//   Initializing: {screen:Initializing},
+//   Authorized: {screen:Authorized},
+//   Unauthorized: {screen: Unauthorized}
+// })
 
 const styles = StyleSheet.create({
   container: {
@@ -57,3 +54,5 @@ const styles = StyleSheet.create({
     paddingHorizontal: 14
   }
 });
+
+export default withAuthenticator(App)
--- testing/MyProject-day2/index.js
+++ testing/MyProject-day2/index.js
@@ -4,6 +4,10 @@ import {AppRegistry, YellowBox} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
+import Amplify from "aws-amplify";
+import config from "./aws-exports";
+Amplify.configure(config);
+
 YellowBox.ignoreWarnings(["Remote debugger"])
 
 AppRegistry.registerComponent(appName, () => App);
